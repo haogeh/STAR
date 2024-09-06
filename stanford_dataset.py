3e2be5c76dcc1b55eb87f3976fd2ada3f63a65fb
haogeh <haogeh@stanford.edu>
2024-09-06 16:53:35 -0700

    FEAT: correct model_config

--- stanford_dataset.py
+++ stanford_dataset.py
@@ -27,7 +27,7 @@ def generate_annotation_line(path, points_5pt, points, scale, center_w, center_h
 # %%
 class Data_Config:
     def __init__(self):
-            self.data_definition = "300W"
+            self.data_definition = "stanford"
             
             self.train_tsv_file = 'stanford_dataset/annot/train.tsv'
             self.val_tsv_file = None
@@ -44,28 +44,27 @@ class Data_Config:
             self.channels = 3
             self.means = (127.5, 127.5, 127.5)
             self.scale = 0.00784313725490196
-            self.classes_num = [68, 9, 68]
+            self.classes_num = [28, 2, 28]
             self.crop_op = True
             self.aug_prob = 1.0
             self.label_num = 12
             self.edge_info = (
-                (False, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)),  # FaceContour
-                (False, (17, 18, 19, 20, 21)),  # RightEyebrow
-                (False, (22, 23, 24, 25, 26)),  # LeftEyebrow
-                (False, (27, 28, 29, 30)),  # NoseLine
-                (False, (31, 32, 33, 34, 35)),  # Nose
-                (True, (36, 37, 38, 39, 40, 41)),  # RightEye
-                (True, (42, 43, 44, 45, 46, 47)),  # LeftEye
-                (True, (48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59)),  # OuterLip
-                (True, (60, 61, 62, 63, 64, 65, 66, 67)),  # InnerLip
+                (False, (9,10)), 
+                (False, (11,12)),
             )
-            self.flip_mapping = ([0, 16], [1, 15], [2, 14], [3, 13], [4, 12], [5, 11], [6, 10], [7, 9], [17, 26], [18, 25], [19, 24], [20, 23], [21, 22], [31, 35], [32, 34], [36, 45], [37, 44], [38, 43], [39, 42], [40, 47], [41, 46], [48, 54], [49, 53], [50, 52], [61, 63], [60, 64], [67, 65], [58, 56], [59, 55])
+            self.flip_mapping = (
+                [0, 1],[2, 3], 
+                [5, 6],[7, 8],
+                [9 ,10],[11,12],
+                [14,15],[17,18],
+                [21,22],[24,25],   
+                )
             self.encoder_type = 'default'
 
             # Val
             self.norm_type = 'default'
-            self.nme_left_index = 36
-            self.nme_right_index = 45
+            self.nme_left_index = 9
+            self.nme_right_index = 10
 data_config = Data_Config()
 
 # %%
@@ -104,8 +103,12 @@ for psd_file in psd_files:
 
     # %%
     points = np.zeros((28,2))
-    points[:2] = get_iris_points(irises[0])
-    points[2:4] = get_iris_points(irises[1])
+    iris_left_points = get_iris_points(irises[0])
+    iris_right_points = get_iris_points(irises[1])
+    points[0] = iris_left_points[0]
+    points[1] = iris_right_points[1]
+    points[2] = iris_left_points[1]
+    points[3] = iris_right_points[0]
     points[4:] = np.array([get_ctr(layer.bbox) for layer in layers[2:]])
 
     points_5pt = np.zeros((5,2))
@@ -162,9 +165,10 @@ for psd_file in psd_files:
 
     # %%
     mrn,dsc = re.match(r'MRN(\d+)_DSC_?(\d+)', psd_file).groups()
-    image_path = os.path.join(data_config.train_pic_dir, f"MRN{mrn}_DSC{dsc}.jpg")
-    annotation_line = generate_annotation_line(image_path, points_5pt, points, scale, center_w, center_h)
-    cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+    image_name = f"MRN{mrn}_DSC{dsc}.jpg"
+    image_path = os.path.join(data_config.train_pic_dir, image_name)
+    annotation_line = generate_annotation_line(image_name, points_5pt, points, scale, center_w, center_h)
+    # cv2.imwrite(image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
     annotation_lines.append(annotation_line)
     # %%
     # Plot the Cardinal Line
@@ -187,3 +191,4 @@ for psd_file in psd_files:
 with open(data_config.train_tsv_file, 'w') as f:
     f.write("\n".join(annotation_lines))
 # %%
+# Dataloader
